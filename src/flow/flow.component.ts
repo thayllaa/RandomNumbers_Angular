356a0906b75044675a710a05702413ccb3835228
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-03-08 22:05:43 -0300

    2 files added

---
+++ src/flow/flow.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flow',
+  templateUrl: './flow.component.html',
+  styleUrls: ['./flow.component.css']
+})
+export class FlowComponent implements OnInit {
+  n: number = 10;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getNumbers() {
+    let arr = [];
+    for (let i = 1; i <= this.n; i++) {
+      arr.push(Math.trunc(100 * Math.random()));
+    }
+    return arr;
+  }
+
+}
