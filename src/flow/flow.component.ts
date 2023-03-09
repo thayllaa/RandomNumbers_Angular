8b3e28e8d51723afb532deef8838321aa5261cdd
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-03-08 22:10:11 -0300

    1 file modified

--- src/flow/flow.component.ts
+++ src/flow/flow.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-flow',
   standalone: true,
-  imports: [ CommonModule],
+  imports: [ CommonModule, FormsModule ],
   templateUrl: './flow.component.html',
   styleUrls: ['./flow.component.css']
 })
